17e3c5ceb30c922e541b893af2fabf20c73588b8
Caio Emidio <caio.emidio@gmail.com>
2024-01-21 21:22:01 +0000

    Included Meta

---
+++ layout/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <title>Keep Clean Ireland</title>
+  <meta charset="utf-8" />
+  <meta
+    name="description"
+    content="Keep Clean is a leading B2B cleaning company in Ireland. We provide tailor-made cleaning solutions to Irish businesses"
+  />
+  <meta name="author" content="Keep Clean Ireland" />
+
+  <!-- Twitter tags -->
+  <meta name="twitter:title" content="Keep Clean Ireland" />
+  <meta
+    name="twitter:description"
+    content="Keep Clean is a leading B2B cleaning company in Ireland. We provide tailor-made cleaning solutions to Irish businesses"
+  />
+  <meta
+    name="twitter:site"
+    content="@B2B cleaning company Keep Clean Cleaning industry revolution Professional cleaning services Environmental sustainability Customer service excellence Quality of life improvement Environmental and social responsibility Clean and pleasant environments Well-maintained environment Productivity enhancement Overall well-being Positive impact cleaning Service quality reference Customer care excellence Impeccable cleaning solutions Continuous improvement in cleaning Leading cleaning company Evolving cleaning needs Dublin cleaning services"
+  />
+  <meta
+    name="twitter:creator"
+    content="@B2B cleaning company Keep Clean Cleaning industry revolution Professional cleaning services Environmental sustainability Customer service excellence Quality of life improvement Environmental and social responsibility Clean and pleasant environments Well-maintained environment Productivity enhancement Overall well-being Positive impact cleaning Service quality reference Customer care excellence Impeccable cleaning solutions Continuous improvement in cleaning Leading cleaning company Evolving cleaning needs Dublin cleaning services"
+  />
+  <meta
+    property="twitter:image"
+    content="https://www.keepcleanireland.ie/_nuxt/static/about_us.jpg"
+  />
+  <meta property="twitter:image:width" content="1200" />
+  <meta property="twitter:image:height" content="600" />
+
+  <!-- Open Graph / Facebook tags-->
+  <meta name="og:title" content="Keep Clean Ireland" />
+  <meta
+    name="og:description"
+    content="Keep Clean is a leading B2B cleaning company in Ireland. We provide tailor-made cleaning solutions to Irish businesses"
+  />
+  <meta property="og:url" content="https://www.keepcleanireland.ie/" />
+  <meta property="og:site_name" content="Keep Clean Ireland" />
+  <meta
+    property="og:image"
+    content="https://www.keepcleanireland.ie/_nuxt/static/about_us.jpg"
+  />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="600" />
+</template>
